a87a9ea003906430a7102968ccc33f2c41bd644b
nayeli839 <nayelisilva839@gmail.com>
2023-04-19 16:29:31 -0300

    -q

---
+++ kanban-fire/src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'kabanfire-fbf3a',
+    appId: '1:344327707300:web:f793fafe861c013e086431',
+    storageBucket: 'kabanfire-fbf3a.appspot.com',
+    apiKey: 'AIzaSyDlW97LiGrM7TwEAxKIeImpUBH0NkJviz4',
+    authDomain: 'kabanfire-fbf3a.firebaseapp.com',
+    messagingSenderId: '344327707300',
+    measurementId: 'G-WKBQEY5DFE',
+  },};
---
+++ kanban-fire/src/environments/environment.ts
@@ -0,0 +1,22 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDlW97LiGrM7TwEAxKIeImpUBH0NkJviz4",
+    authDomain: "kabanfire-fbf3a.firebaseapp.com",
+    projectId: "kabanfire-fbf3a",
+    storageBucket: "kabanfire-fbf3a.appspot.com",
+    messagingSenderId: "344327707300",
+    appId: "1:344327707300:web:f793fafe861c013e086431",
+    measurementId: "G-WKBQEY5DFE"
+  };
+  export const environment = {
+    production: false,
+  firebase: {
+    projectId: 'kabanfire-fbf3a',
+    appId: '1:344327707300:web:f793fafe861c013e086431',
+    storageBucket: 'kabanfire-fbf3a.appspot.com',
+    apiKey: 'AIzaSyDlW97LiGrM7TwEAxKIeImpUBH0NkJviz4',
+    authDomain: 'kabanfire-fbf3a.firebaseapp.com',
+    messagingSenderId: '344327707300',
+    measurementId: 'G-WKBQEY5DFE',
+  }
+  };
